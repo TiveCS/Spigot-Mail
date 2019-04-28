cc669a4cc2715ad0ee25ad3fc7dce63a72928d5f
Ahmad Fathan <41243950+TiveCS@users.noreply.github.com>
2019-04-29 04:51:46 +0700

    Add files via upload

--- DiaMail - v2.4/src/team/creativecode/diamail/cmds/DiamailAdminCmd.java
+++ DiaMail - v2.4/src/team/creativecode/diamail/cmds/DiamailAdminCmd.java
@@ -3,11 +3,15 @@ package team.creativecode.diamail.cmds;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+
+import team.creativecode.diamail.manager.AdminManager;
 
 public class DiamailAdminCmd implements CommandExecutor, TabCompleter {
 
@@ -16,9 +20,14 @@ public class DiamailAdminCmd implements CommandExecutor, TabCompleter {
 		if (cmd.getName().equalsIgnoreCase("diamailadmin")) {
 			if (args.length == 1) {
 				if (sender instanceof Player) {
-					// Player p = (Player) sender;
-					// AdminManager admin = new AdminManager(p);
+					Player p = (Player) sender;
+					AdminManager admin = new AdminManager(p);
 					if (args[0].equalsIgnoreCase("physicalMailbox")) {
+						Location loc = p.getLocation();
+						loc.setY(loc.getY() - 1);
+						if (!loc.getBlock().getType().equals(Material.AIR)) {
+							admin.physicalMailbox(loc.getBlock());
+						}
 						return true;
 					}
 				}
